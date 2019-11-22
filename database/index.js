e61a38d33fe38a6e056508da4b584bd04b4844da
Jyumi <justinlu75@gmail.com>
2019-11-21 18:34:59 -0800

    added solutions

--- database/index.js
+++ database/index.js
@@ -1,19 +1,9 @@
 const mysql = require('mysql');
 
-// TODO: establish connection
-var db = mysql.createConnection({ //establishes connection
+const connection = mysql.createconnection({
     user: 'root',
     password: '',
     database: 'ReviewDB'
-});
-
-db.connect(function(err){ //test connection
-    if(err) {
-        console.error('error connecting: ' + err.stack);
-        return
-    }
-
-    console.log('connected to DB')
-});
-
-module.exports = db; //exports this for use with anything else node.js related
+})
+// TODO: establish connection
+module.exports = connection
